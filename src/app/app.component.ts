4ab86c34adf9ee966981e7ca6afe14ca403fb9c1
viggy <dviglione02@gmail.com>
2020-04-09 18:59:16 -0400

    reproducing mat-checkbox checked bug

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef  } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'custom-controls';
+
+  @ViewChild('ref', {static: false}) el: ElementRef;
+
+  doSomething(e) {
+    // this reads false when you check the checkbox and true when you uncheck the check box:
+    console.log('checkbox checked? ', e.target.querySelector('input[type="checkbox"]').checked);
+
+    // I can't even use ViewChild with mat-checkbox. It is undefined:
+    // 'cannot read properry nativeElement of undefined'
+    console.log('the native element: ', this.el.nativeElement);
+
+  }
 }
